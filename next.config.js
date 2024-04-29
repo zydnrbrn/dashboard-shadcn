1bc134c31dd520e4fa433f8f87d0163fd41a8fcf
zydnrbrn <zidanreborn@gmail.com>
2024-04-29 17:32:42 +0700

    added: pwa support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,      // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
   images: {
     domains: ["utfs.io"],
   },
 };
 
-module.exports = nextConfig;
+// PWA Configuration
+const withPWA = require("next-pwa")({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
+
+module.exports = withPWA(nextConfig);
